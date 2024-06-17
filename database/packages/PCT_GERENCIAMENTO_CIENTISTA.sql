436fb0471a520aa3bb88afafb32c6362b57e0ea3
Arthur Reis <99774782+arthurb-reis@users.noreply.github.com>
2024-06-17 15:46:33 -0300

    Update PCT_GERENCIAMENTO_CIENTISTA.sql

--- database/packages/PCT_GERENCIAMENTO_CIENTISTA.sql
+++ database/packages/PCT_GERENCIAMENTO_CIENTISTA.sql
@@ -1,9 +1,7 @@
--- Declaração de pacote para manipulações de usuário
+-- Declaração de pacote para gerenciamento do cientista
 CREATE OR REPLACE PACKAGE PCT_GERENCIAMENTO_CIENTISTA AS
     E_USER_NAO_CADASTRADO EXCEPTION;
     E_NAO_EH_CIENTISTA EXCEPTION;
-    --E_ESTRELA_INVALIDA EXCEPTION;
-    --E_ESTRELA_NAO_EXISTE EXCEPTION;
     FUNCTION insere_estrela (
         p_cpi LIDER.CPI%TYPE,
         p_id ESTRELA.ID_ESTRELA%TYPE,
@@ -18,7 +16,7 @@ CREATE OR REPLACE PACKAGE PCT_GERENCIAMENTO_CIENTISTA AS
         p_cpi LIDER.CPI%TYPE,
         p_id ESTRELA.ID_ESTRELA%TYPE
     ) RETURN VARCHAR2;
-    /*FUNCTION remove_estrela_por_posicao (
+    FUNCTION remove_estrela_por_posicao (
         p_cpi LIDER.CPI%TYPE,
         p_x ESTRELA.X%TYPE,
         p_y ESTRELA.Y%TYPE,
@@ -36,7 +34,6 @@ CREATE OR REPLACE PACKAGE PCT_GERENCIAMENTO_CIENTISTA AS
     ) RETURN VARCHAR2;
     FUNCTION update_estrela (
         p_cpi LIDER.CPI%TYPE,
-        p_id_antigo ESTRELA.ID_ESTRELA%TYPE,
         p_id ESTRELA.ID_ESTRELA%TYPE,
         p_nome ESTRELA.NOME%TYPE,
         p_classificacao ESTRELA.CLASSIFICACAO%TYPE,
@@ -44,7 +41,7 @@ CREATE OR REPLACE PACKAGE PCT_GERENCIAMENTO_CIENTISTA AS
         p_x ESTRELA.X%TYPE,
         p_y ESTRELA.Y%TYPE,
         p_z ESTRELA.Z%TYPE
-    ) RETURN VARCHAR2;*/
+    ) RETURN VARCHAR2;
 END PCT_GERENCIAMENTO_CIENTISTA;
 
 /
@@ -114,8 +111,114 @@ CREATE OR REPLACE PACKAGE BODY PCT_GERENCIAMENTO_CIENTISTA AS
         WHEN NO_DATA_FOUND THEN
             RAISE_APPLICATION_ERROR(-20000, 'Estrela com esse id não existe');
         WHEN OTHERS THEN
-            RAISE_APPLICATION_ERROR(-20000, 'Não foi possível inserir estrela.');
+            RAISE_APPLICATION_ERROR(-20000, 'Não foi possível remover estrela.');
     END remove_estrela_por_id;
+
+    FUNCTION remove_estrela_por_posicao (
+        p_cpi LIDER.CPI%TYPE,
+        p_x ESTRELA.X%TYPE,
+        p_y ESTRELA.Y%TYPE,
+        p_z ESTRELA.Z%TYPE
+    ) RETURN VARCHAR2 IS
+        v_estrela ESTRELA.ID_ESTRELA%TYPE;
+    BEGIN
+        IF (NOT retorna_se_cientista(p_cpi)) THEN
+            RAISE E_NAO_EH_CIENTISTA;
+        END IF;
+        SELECT ID_ESTRELA INTO v_estrela FROM ESTRELA WHERE X = p_x AND Y = p_y AND Z = p_z;
+        DELETE FROM ESTRELA WHERE X = p_x AND Y = p_y AND Z = p_z;
+        RETURN 'Estrela removida com sucesso.';
+    EXCEPTION
+        WHEN E_USER_NAO_CADASTRADO THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não cadastrado');
+        WHEN E_NAO_EH_CIENTISTA THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não é cientista');
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Estrela nessa posição não existe');
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Não foi possível remover estrela.');
+    END remove_estrela_por_posicao;
+
+    FUNCTION ler_estrela_por_id (
+        p_cpi LIDER.CPI%TYPE,
+        p_id ESTRELA.ID_ESTRELA%TYPE
+    ) RETURN VARCHAR2 IS
+        v_estrela ESTRELA%ROWTYPE;
+        v_saida VARCHAR2(32767) := 'ID_ESTRELA;NOME;NOME;CLASSIFICACAO;MASSA;X;Y;Z' || CHR(10);
+    BEGIN
+        IF (NOT retorna_se_cientista(p_cpi)) THEN
+            RAISE E_NAO_EH_CIENTISTA;
+        END IF;
+        SELECT * INTO v_estrela FROM ESTRELA WHERE ID_ESTRELA = p_id;
+        v_saida := v_saida || v_estrela.id_estrela || ';' || v_estrela.nome || ';' || v_estrela.classificacao || ';' || v_estrela.massa || ';' || v_estrela.x || ';' || v_estrela.y || ';' || v_estrela.z || CHR(10);
+        RETURN v_saida;
+    EXCEPTION
+        WHEN E_USER_NAO_CADASTRADO THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não cadastrado');
+        WHEN E_NAO_EH_CIENTISTA THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não é cientista');
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Estrela com esse id não existe');
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Não foi possível ler estrela.');
+    END ler_estrela_por_id;
+
+    FUNCTION ler_estrela_por_posicao (
+        p_cpi LIDER.CPI%TYPE,
+        p_x ESTRELA.X%TYPE,
+        p_y ESTRELA.Y%TYPE,
+        p_z ESTRELA.Z%TYPE
+    ) RETURN VARCHAR2 IS
+        v_estrela ESTRELA%ROWTYPE;
+        v_saida VARCHAR2(32767) := 'ID_ESTRELA;NOME;NOME;CLASSIFICACAO;MASSA;X;Y;Z' || CHR(10);
+    BEGIN
+        IF (NOT retorna_se_cientista(p_cpi)) THEN
+            RAISE E_NAO_EH_CIENTISTA;
+        END IF;
+        SELECT * INTO v_estrela FROM ESTRELA WHERE X = p_x AND Y = p_y AND Z = p_z;
+        v_saida := v_saida || v_estrela.id_estrela || ';' || v_estrela.nome || ';' || v_estrela.classificacao || ';' || v_estrela.massa || ';' || v_estrela.x || ';' || v_estrela.y || ';' || v_estrela.z || CHR(10);
+        RETURN v_saida;
+    EXCEPTION
+        WHEN E_USER_NAO_CADASTRADO THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não cadastrado');
+        WHEN E_NAO_EH_CIENTISTA THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não é cientista');
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Estrela com esse id não existe');
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Não foi possível ler estrela.');
+    END ler_estrela_por_posicao;
+
+    FUNCTION update_estrela (
+        p_cpi LIDER.CPI%TYPE,
+        p_id ESTRELA.ID_ESTRELA%TYPE,
+        p_nome ESTRELA.NOME%TYPE,
+        p_classificacao ESTRELA.CLASSIFICACAO%TYPE,
+        p_massa ESTRELA.MASSA%TYPE,
+        p_x ESTRELA.X%TYPE,
+        p_y ESTRELA.Y%TYPE,
+        p_z ESTRELA.Z%TYPE
+    ) RETURN VARCHAR2 IS
+        v_estrela ESTRELA.ID_ESTRELA%TYPE;
+    BEGIN
+        IF (NOT retorna_se_cientista(p_cpi)) THEN
+            RAISE E_NAO_EH_CIENTISTA;
+        END IF;
+        SELECT ID_ESTRELA INTO v_estrela FROM ESTRELA WHERE ID_ESTRELA = p_id;
+        UPDATE ESTRELA SET nome = p_nome, classificacao = p_classificacao, massa = p_massa, x = p_x, y = p_y, z = p_z WHERE id_estrela = p_id;
+        RETURN 'Estrela atualizada com sucesso.';
+    EXCEPTION
+        WHEN E_USER_NAO_CADASTRADO THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não cadastrado.');
+        WHEN E_NAO_EH_CIENTISTA THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não é cientista.');
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Estrela com esse id não existe.');
+        WHEN DUP_VAL_ON_INDEX THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Já existe estrela nessa posição.');
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Não foi possível atualizar estrela.');
+    END update_estrela;
     
 END PCT_GERENCIAMENTO_CIENTISTA;
 /
