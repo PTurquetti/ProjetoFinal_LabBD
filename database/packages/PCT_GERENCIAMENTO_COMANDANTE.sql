564435e1f28648400ea7b7c2cfb6805f9c1749bf
Arthur Reis <99774782+arthurb-reis@users.noreply.github.com>
2024-06-17 16:18:18 -0300

    Update PCT_GERENCIAMENTO_COMANDANTE.sql

--- database/packages/PCT_GERENCIAMENTO_COMANDANTE.sql
+++ database/packages/PCT_GERENCIAMENTO_COMANDANTE.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE PACKAGE PCT_GERENCIAMENTO_COMANDANTE AS
     E_USER_NAO_EH_COMANDANTE EXCEPTION;
     E_NACAO_JA_TEM_FEDERACAO EXCEPTION;
     E_NACAO_NAO_TEM_FEDERACAO EXCEPTION;
-    --E_PLANETA_JA_DOMINADO EXCEPTION;
+    E_PLANETA_JA_DOMINADO EXCEPTION;
     FUNCTION inserir_federacao (
         p_cpiuser LIDER.CPI%TYPE,
         p_federacao FEDERACAO.NOME%TYPE
@@ -12,10 +12,10 @@ CREATE OR REPLACE PACKAGE PCT_GERENCIAMENTO_COMANDANTE AS
     FUNCTION excluir_federacao (
         p_cpiuser LIDER.CPI%TYPE
     ) RETURN VARCHAR2;
-    /*FUNCTION insere_dominancia (
+    FUNCTION insere_dominancia (
         p_cpiuser LIDER.CPI%TYPE,
         p_planeta PLANETA.ID_ASTRO%TYPE
-    ) RETURN VARCHAR2;*/
+    ) RETURN VARCHAR2;
 END PCT_GERENCIAMENTO_COMANDANTE;
 
 /
@@ -114,5 +114,46 @@ CREATE OR REPLACE PACKAGE BODY PCT_GERENCIAMENTO_COMANDANTE AS
         WHEN OTHERS THEN
             RAISE_APPLICATION_ERROR(-20000, 'Não foi possível inserir federação.');
     END excluir_federacao;
+
+    FUNCTION insere_dominancia (
+        p_cpiuser LIDER.CPI%TYPE,
+        p_planeta PLANETA.ID_ASTRO%TYPE
+    ) RETURN VARCHAR2 IS
+        v_nacao NACAO.NOME%TYPE;
+        v_inicio_dominancia DOMINANCIA.DATA_INI%TYPE;
+        v_fim_dominancia DOMINANCIA.DATA_FIM%TYPE;
+        v_data_atual DATE := SYSDATE; 
+        v_count NUMBER;
+    BEGIN
+        v_nacao := retorna_nacao(p_cpiuser);
+        IF v_nacao IS NULL THEN
+            RAISE E_USER_NAO_CADASTRADO;
+        END IF;
+        IF (NOT retorna_se_comandante(p_cpiuser)) THEN
+            RAISE E_USER_NAO_EH_COMANDANTE;
+        END IF;
+        SELECT COUNT(*) INTO v_count FROM DOMINANCIA WHERE PLANETA = p_planeta;
+        IF v_count > 0 THEN
+            SELECT DATA_INI, DATA_FIM INTO v_inicio_dominancia, v_fim_dominancia FROM DOMINANCIA WHERE PLANETA = p_planeta;
+            -- Lógica para determinar se o planeta já está dominado (considera-se que não pode inserir dominações futuras)
+            IF v_inicio_dominancia IS NOT NULL THEN -- Se tem dado de dominância
+                IF v_fim_dominancia IS NULL OR v_fim_dominancia > v_data_atual THEN -- Se dominância não acabou ainda
+                    RAISE E_PLANETA_JA_DOMINADO;
+                END IF;
+            END IF;
+        END IF;
+        INSERT INTO DOMINANCIA (PLANETA, NACAO, DATA_INI) VALUES (p_planeta, v_nacao, v_data_atual);
+        RETURN 'Dominância inserida com sucesso!';
+    EXCEPTION 
+        WHEN E_USER_NAO_CADASTRADO THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não cadastrado.');
+        WHEN E_USER_NAO_EH_COMANDANTE THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não é comandante.');
+        WHEN E_PLANETA_JA_DOMINADO THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Planeta já está dominado.');
+        --WHEN OTHERS THEN
+            --RAISE_APPLICATION_ERROR(-20000, 'Não foi possível inserir dominância.');
+    END insere_dominancia;
+    
 END PCT_GERENCIAMENTO_COMANDANTE;
 /
