3abcb87315c506be43ac11de1fd37d71ad43f869
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2024-06-16 21:52:21 -0300

    Otimização da conexão

--- app/db_controller.py
+++ app/db_controller.py
@@ -10,26 +10,32 @@ class DBController:
         self.port = os.getenv('port')
         self.service_name = os.getenv('service_name')
         self.password = os.getenv('password')
+        self.connection = oracledb.connect(user=self.user, password=self.password, host=self.host, port=self.port, service_name=self.service_name)
     
+    def __del__(self):
+        self.connection.close()
+
     def __map_function_return_type(self, return_type):
         if return_type in [int, float]:
             return oracledb.NUMBER
         if return_type == str:
             return oracledb.STRING
+    
+    def commit(self):
+        self.connection.commit()
+    
+    def rollback(self):
+        self.connection.rollback()
         
     def call_function(self, function_name, function_parameters, return_type):
         mapped_return_type = self.__map_function_return_type(return_type)
         converted_function_name = f'a11796893.{function_name}'
-        with oracledb.connect(user=self.user, password=self.password, host=self.host, port=self.port, service_name=self.service_name) as connection:
-            cursor = connection.cursor()
-            try:
-                output = cursor.callfunc(converted_function_name, mapped_return_type, function_parameters)
-                connection.commit()
-                cursor.close()
-                connection.close()
-                return output
-            except Exception as e:
-                connection.rollback()
-                cursor.close()
-                connection.close()
-                raise e
+        # with oracledb.connect(user=self.user, password=self.password, host=self.host, port=self.port, service_name=self.service_name) as connection:
+        cursor = self.connection.cursor()
+        try:
+            output = cursor.callfunc(converted_function_name, mapped_return_type, function_parameters)
+            cursor.close()
+            return output
+        except Exception as e:
+            cursor.close()
+            raise e
