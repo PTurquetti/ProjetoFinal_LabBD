637af035bb9322fcc0e5b3a75cb6a8401337e1cc
Arthur Reis <99774782+arthurb-reis@users.noreply.github.com>
2024-06-17 10:34:52 -0300

    Update PCT_GERENCIAMENTO_LIDER.sql
    Adicionada a função de alterar líder e exceptions padronizados

--- database/packages/PCT_GERENCIAMENTO_LIDER.sql
+++ database/packages/PCT_GERENCIAMENTO_LIDER.sql
@@ -1,27 +1,27 @@
 -- Declaração de pacote para manipulações de usuário
 CREATE OR REPLACE PACKAGE PCT_GERENCIAMENTO_LIDER AS
     E_USER_NAO_EH_LIDER EXCEPTION;
-    /*E_LIDER_JA_EH_LIDER_DE_OUTRA_FACCAO EXCEPTION;
-    E_COMUNIDADE_JA_PARTICIPA EXCEPTION;
+    E_LIDER_JA_EH_LIDER_DE_OUTRA_FACCAO EXCEPTION;
+    /*E_COMUNIDADE_JA_PARTICIPA EXCEPTION;
     E_COMUNIDADE_INVALIDA EXCEPTION;
     E_FACCAO_NAO_PERTENCE_A_NACAO EXCEPTION;*/
-    PROCEDURE alterar_nome_faccao (
+    FUNCTION alterar_nome_faccao (
         p_cpilider LIDER.CPI%TYPE,
         p_novo_nome FACCAO.NOME%TYPE
-    );
-    /*PROCEDURE indica_lider (
+    ) RETURN VARCHAR2;
+    FUNCTION indica_lider (
         p_cpilider LIDER.CPI%TYPE,
         p_cpi_novo_lider LIDER.CPI%TYPE
-    );
-    PROCEDURE credenciar_comunidade (
+    ) RETURN VARCHAR2;
+    /*FUNCTION credenciar_comunidade (
         p_cpilider LIDER.CPI%TYPE,
         p_especie_comunidade COMUNIDADE.NOME%TYPE, 
         p_nome_comunidade COMUNIDADE.NOME%TYPE
-    );
-    PROCEDURE remove_faccao_de_nacao (
+    ) RETURN VARCHAR2;
+    FUNCTION remove_faccao_de_nacao (
         p_cpilider LIDER.CPI%TYPE,
         p_nacao NACAO.NOME%TYPE
-    );*/
+    ) RETURN VARCHAR2;*/
 END PCT_GERENCIAMENTO_LIDER;
 
 /
@@ -40,10 +40,10 @@ CREATE OR REPLACE PACKAGE BODY PCT_GERENCIAMENTO_LIDER AS
             RETURN null;
     END retorna_faccao;
 
-    PROCEDURE alterar_nome_faccao (
+    FUNCTION alterar_nome_faccao (
         p_cpilider LIDER.CPI%TYPE,
         p_novo_nome FACCAO.NOME%TYPE
-    ) IS
+    ) RETURN VARCHAR2 IS
         v_faccao FACCAO.NOME%TYPE; -- Facção do líder
     BEGIN
         v_faccao := retorna_faccao(p_cpilider); -- Retorna a facção do líder
@@ -52,16 +52,46 @@ CREATE OR REPLACE PACKAGE BODY PCT_GERENCIAMENTO_LIDER AS
         END IF;
         UPDATE FACCAO SET NOME = p_novo_nome WHERE NOME = v_faccao; -- Update no nome
         -- Esse update aciona o trigger de update de nome de facção, pois existem tabelas que precisam ser alteradas também.
-        DBMS_OUTPUT.PUT_LINE('Nome alterado com sucesso!');
+        RETURN 'Nome alterado com sucesso!';
     EXCEPTION
         WHEN E_USER_NAO_EH_LIDER THEN
-            DBMS_OUTPUT.PUT_LINE('Usuário não é líder de nenhuma facção.');
+            RAISE_APPLICATION_ERROR(-20000, 'Usuário não é líder de nenhuma facção.');
         WHEN DUP_VAL_ON_INDEX THEN
-            DBMS_OUTPUT.PUT_LINE('Já existe uma facção com esse nome.');
+            RAISE_APPLICATION_ERROR(-20000, 'Já existe uma facção com esse nome.');
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('Não foi possível mudar o nome da facção. ERRO: ' || SQLCODE);
+            RAISE_APPLICATION_ERROR(-20000, 'Não foi possível mudar o nome da facção.');
     END alterar_nome_faccao;
 
+    FUNCTION indica_lider (
+        p_cpilider LIDER.CPI%TYPE,
+        p_cpi_novo_lider LIDER.CPI%TYPE
+    ) RETURN VARCHAR2 IS
+        v_faccao_lider FACCAO.NOME%TYPE;
+        v_faccao_outro_lider FACCAO.NOME%TYPE;
+    BEGIN
+        v_faccao_lider := retorna_faccao(p_cpilider);
+        v_faccao_outro_lider := retorna_faccao(p_cpi_novo_lider);
+        IF v_faccao_lider IS NULL THEN
+            RAISE E_USER_NAO_EH_LIDER;
+        END IF;
+        IF v_faccao_outro_lider IS NOT NULL THEN
+            RAISE E_LIDER_JA_EH_LIDER_DE_OUTRA_FACCAO;
+        END IF;
+        UPDATE FACCAO SET LIDER = p_cpi_novo_lider WHERE LIDER = p_cpilider;
+        RETURN 'Líder alterado com sucesso!';
+        EXCEPTION
+            WHEN E_USER_NAO_EH_LIDER THEN
+                RAISE_APPLICATION_ERROR(-20000, 'Usuário não é líder de nenhuma facção.');
+            WHEN E_LIDER_JA_EH_LIDER_DE_OUTRA_FACCAO THEN
+                RAISE_APPLICATION_ERROR(-20000, 'Líder indicado já é líder de outra facção.');
+            WHEN DUP_VAL_ON_INDEX THEN
+                RAISE_APPLICATION_ERROR(-20000, 'Líder indicado já é líder de outra facção.');
+            WHEN OTHERS THEN
+                IF SQLCODE = -2291 THEN
+                    RAISE_APPLICATION_ERROR(-20000, 'Líder não está cadastrado.');
+                END IF;
+                RAISE_APPLICATION_ERROR(-20000, 'Não foi possível alterar o lider da facção.');
+    END indica_lider;
     
 END PCT_GERENCIAMENTO_LIDER;
 /
