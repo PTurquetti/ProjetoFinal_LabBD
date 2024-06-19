0a382478d5a510e52ef2b008cbc8e4130bc1fb36
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2024-06-18 21:19:28 -0300

    Adição de view para usuário

---
+++ database/views/VIEW_USUARIO_LIDER.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW VIEW_USUARIO_LIDER AS
+    SELECT
+        USERID AS ID,
+        PASSWORD AS PASSWORD,
+        IDLIDER AS CPI,
+        NOME,
+        CARGO,
+        NACAO,
+        ESPECIE
+    FROM
+        USERS U
+    JOIN
+        LIDER L ON U.IDLIDER = L.CPI;
